acf30a9112013e4bc941563cfd777a1829d922ea
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2023-09-30 15:06:14 +0300

    Урок 1

---
+++ index.js
@@ -0,0 +1,31 @@
+const yargs = require("yargs");
+const pkg = require("yargs");
+const { addNote, getNotes, printNotes } = require("./notes.controller");
+
+yargs.version(pkg.version);
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+    // addNotes(title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes();
+  },
+});
+
+yargs.parse();
