cef7555a465f6cce36d096c48c91b6453fc03b5d
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2023-10-24 14:31:09 +0300

    с докером

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const chalk = require("chalk");
 const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const {
   addNote,
   getNotes,
   removeNote,
   updateNote,
 } = require("./notes.controller");
+const {
+  addUser,
+  loginUser,
+  // removeNote,
+  // updateNote,
+} = require("./users.controller");
+const auth = require("./middlewares/auth");
 
-const port = 3000;
+const port = 3009;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,16 +26,71 @@ app.set("views", "pages");
 
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
+app.get("/login", async (req, res) => {
+  res.render("login", {
+    title: "Express App",
+    error: undefined,
+  });
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const token = await loginUser(req.body.email, req.body.password);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/");
+  } catch (e) {
+    res.render("login", {
+      title: "Express App",
+      error: e.message,
+    });
+  }
+});
+
+app.get("/register", async (req, res) => {
+  res.render("register", {
+    title: "Express App",
+    error: undefined,
+  });
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    await addUser(req.body.email, req.body.password);
+    res.redirect("/login");
+  } catch (e) {
+    if (e.code === 11000) {
+      res.render("register", {
+        title: "Express App",
+        error: "Email is already registered",
+      });
+      return;
+    }
+    console.log(e);
+    res.render("register", {
+      title: "Express App",
+      error: e.message,
+    });
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "", { httpOnly: true });
+  res.redirect("/login");
+});
+app.use(auth);
+
 app.get("/", async (req, res) => {
   res.render("index", {
     title: "Express App",
     notes: await getNotes(),
+    userEmail: req.user.email,
     created: false,
     error: false,
   });
@@ -34,10 +98,11 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   try {
-    await addNote(req.body.title);
+    await addNote(req.body.title, req.user.email);
     res.render("index", {
       title: "Express App",
       notes: await getNotes(),
+      userEmail: req.user.email,
       created: true,
       error: false,
     });
@@ -46,6 +111,7 @@ app.post("/", async (req, res) => {
     res.render("index", {
       title: "Express App",
       notes: await getNotes(),
+      userEmail: req.user.email,
       created: false,
       error: true,
     });
@@ -53,31 +119,49 @@ app.post("/", async (req, res) => {
 });
 
 app.delete("/:id", async (req, res) => {
-  await removeNote(req.params.id);
-  res.render("index", {
-    title: "Express App",
-    notes: await getNotes(),
-    created: false,
-    error: false,
-  });
+  try {
+    await removeNote(req.params.id);
+    res.render("index", {
+      title: "Express App",
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: false,
+    });
+  } catch (e) {
+    res.render("index", {
+      title: "Express App",
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: e.message,
+    });
+  }
 });
 
 app.put("/:id", async (req, res) => {
-  await updateNote({ id: req.params.id, title: req.body.title });
-  res.render("index", {
-    title: "Express App",
-    notes: await getNotes(),
-    created: false,
-    error: false,
-  });
+  try {
+    await updateNote({ id: req.params.id, title: req.body.title });
+    res.render("index", {
+      title: "Express App",
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: false,
+    });
+  } catch (e) {
+    res.render("index", {
+      title: "Express App",
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: e.message,
+    });
+  }
 });
 
-mongoose
-  .connect(
-    "mongodb+srv://martadorohova:qweqwe123@cluster0.qteawng.mongodb.net/notes?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    app.listen(port, () => {
-      console.log(chalk.green(`Server has been started on port ${port}...`));
-    });
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
+  app.listen(port, () => {
+    console.log(chalk.green(`Server has been started on port ${port}...`));
   });
+});
